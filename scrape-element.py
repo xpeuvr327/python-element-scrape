8d3dc4c0b843983ed911bc591c4e3ef9db48940d
xpeuvr327 <150825691+xpeuvr327@users.noreply.github.com>
2024-09-30 11:26:23 +0200

    remove duplicate

--- scrape-element.py
+++ scrape-element.py
@@ -4,20 +4,7 @@ import lxml
 
 element = input('What element would you want to know the name of? ')
 url = f'https://www.rsc.org/periodic-table/element/{element}/'
-res = requests.get(url)
-soup = bs4.BeautifulSoup(res.text, 'lxml')
-div = soup.find('span', {"class": "element_header"})
-if div:
-    text = div.text.strip()
-    text = ' '.join(text.split())
-    print(f"The name of the element with atomic number {element} is {text}.")
-else:
-    print("Element not found on the page.")import requests
-import bs4
-import lxml
-
-element = input('What element would you want to know the name of? ')
-url = f'https://www.rsc.org/periodic-table/element/{element}/'
+print('Collecting data...')
 res = requests.get(url)
 soup = bs4.BeautifulSoup(res.text, 'lxml')
 div = soup.find('span', {"class": "element_header"})
